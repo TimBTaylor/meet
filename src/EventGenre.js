14206fe705b71d2e88caf1ef9f9241a45ce3966c
TimBTaylor <79194271+TimBTaylor@users.noreply.github.com>
2021-06-12 16:20:11 -0400

    4.9 exercise

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -1,35 +1,47 @@
 import React, { useEffect, useState } from 'react';
-import { ResponsiveContainer, Pie, PieChart, Cell} from 'recharts';
+import { ResponsiveContainer, Pie, PieChart, Cell } from 'recharts';
 
-const EventGenre = ({ events }) => {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    setData(() => getData());
-  }, [events]);
+export default function EventGenre(props) {
+  const { events } = props
+  const [ data, setData ] = useState([]);
+  const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS']
+  const COLORS = ['#f2a365', '#BBE1FA', '#903749', '#219897', '#616f39'];
 
   const getData = () => {
-    const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'Angular'];
-    const data = genres.map((genre) => {
-      const value = events.filter(({ summary }) => summary.split(' ').includes(genre)).length;
-      return { name: genre, value };
+    let data = genres.map( genre => {
+      const value = events.filter( event => event.summary.replace(',','').replace('.', '').split(' ').includes(genre) ).length
+      return { name: genre, value }
     })
-  return data;
-  };
+    data = data.filter(data => data.value)
+    return data
+  }
 
-  const colors = ['#ee5454', '#5f74f0', '#69eb13', '#13ebeb', '#eb13c7']
+  useEffect(() => {
+    setData(() => getData())
+  }, [events]);
 
   return (
     <ResponsiveContainer height={400}>
       <PieChart>
-        <Pie data={data} cx="47%" cy="50%" labelLine={false} legendType="square" outerRadius={80} fill="#8884d8" datakey="value" label={({ name, percent }) => `${name}` `${(percent * 100).toFixed(0)}%`}>
+        <Pie
+          data={data}
+          cx="50%"
+          cy="50%"
+          labelLine={false}
+          label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
+          outerRadius={80}
+          innerRadius={20}
+          fill="#bbe1fa"
+          stroke="#1b262c"
+          dataKey="value"
+        >
           {
-            data.map((entry, index) => <Cell key={`cell-${index}`} fill={colors[index % colors.length]} name={entry.name}/>)
+            data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={COLORS[index] }/>
+            ))
           }
         </Pie>
       </PieChart>
     </ResponsiveContainer>
-  );
+  )
 }
-
-export default EventGenre;
